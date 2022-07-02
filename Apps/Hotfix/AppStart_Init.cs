c0409a778f7a40de20f0327864330fe4d76a0579
sven <sven9527@qq.com>
2022-07-02 12:09:17 +0800

    DB

--- Apps/Hotfix/AppStart_Init.cs
+++ Apps/Hotfix/AppStart_Init.cs
@@ -42,6 +42,8 @@ namespace ET
             
             Game.Scene.AddComponent<NavmeshComponent>();
 
+            Game.Scene.AddComponent<DBManagerComponent>();
+
             switch (Game.Options.AppType)
             {
                 case AppType.Server:
